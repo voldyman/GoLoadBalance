9fe61b22fd378c6cee51bd4b045d17109a2837d8
Akshay Shekher <voldyman666@gmail.com>
2015-02-19 13:24:19 +0530

    added debug support

--- src/main.go
+++ src/main.go
@@ -1,29 +1,43 @@
 package main
 
 import (
-	"fmt"
 	"flag"
+	"fmt"
+	"runtime"
+	"time"
 )
 
 func main() {
 	var backends_input strings
 
 	var port = flag.Int("port", 8050, "The port to listen for connections")
+	var debug = flag.Bool("debug", false, "Enable debug mode")
 
-	flag.Var(&backends_input ,"backend", "List of backend servers, need at least one")
+	flag.Var(&backends_input, "backend", "List of backend servers, need at least one")
 
 	flag.Parse()
 
 	if flag.NFlag() == 0 || len(backends_input) < 1 {
-        flag.PrintDefaults()
+		flag.PrintDefaults()
 		return
-    } 
+	}
 
 	backends := NewBackends()
 
-	backends.Add([]string(backends_input)  ...)
+	backends.Add([]string(backends_input)...)
+
+	if *debug {
+		go debugRoutine()
+	}
 
 	startServer(*port, backends)
+}
+
+func debugRoutine() {
+	for {
+		<-time.After(2 * time.Second)
+		fmt.Println(time.Now(), "NumGoroutine", runtime.NumGoroutine())
+	}
 }
 
 type strings []string
@@ -32,7 +46,7 @@ func (self *strings) String() string {
 	return fmt.Sprintf("%s", *self)
 }
 
-func (self *strings) Set(val string) error{
+func (self *strings) Set(val string) error {
 
 	*self = append(*self, val)
 
